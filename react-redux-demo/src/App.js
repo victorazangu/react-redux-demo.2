b7912f04958bfd3dd9e94650968752cdfd958d06
victorazangu <victorsazangu@gmail.com>
2023-05-26 20:07:06 +0300

    mastering redux

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+
+function App() {
+	return (
+		<Provider store={store}>
+			<div className="App">
+				<HooksCakeContainer />
+				<CakeContainer />
+			</div>
+		</Provider>
+	);
+}
+
+export default App;
